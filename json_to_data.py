86b72c8d89a8497831082f20618dd4fd1515d77d
Griffin Dietz <smileggd@gmail.com>
2023-11-25 18:27:54 -0700

    updated python script

--- json_to_data.py
+++ json_to_data.py
@@ -2,10 +2,11 @@ import os
 import re
 import csv
 from pathlib import Path
+import datetime
 
 data = open("data.csv", "w")
 data.truncate()
-title = "child__hashed_id,condition,counterbalance,attn1,attn2,test,diverse_desires,implicit_false_belief,diverse_beliefs,diana_false_belief_location,diana_false_belief_control,allie_false_belief_location,num_speakers,coding_experience,smartphone_frequency,smart_speaker_frequency,voice_assistant_frequency\n"
+title = "child__hashed_id,condition,counterbalance,attn1,attn2,test,attn3,rachel_false_belief,diana_false_belief,allie_false_belief,num_speakers,coding_experience,smartphone_frequency,smart_speaker_frequency,voice_assistant_frequency\n"
 data.write(title)
 path = os.path.expanduser("~/Downloads/Guess-What-Happens-Next_framedata_per_session")
 for file in os.listdir(path):
@@ -18,21 +19,23 @@ for file in os.listdir(path):
 			attn1 = 0
 			attn2 = 0
 			test = 0
-			diverse_desires_selected_snack = ""
-			diverse_desires = 0
-			implicit_false_belief = 0
-			diverse_beliefs_selected_location = ""
-			diverse_beliefs = 0 # not yet
-			diana_false_belief_location = 0
-			diana_false_belief_control = 0
-			allie_false_belief_location = 0
+			attn3 = 0
+			rachel_false_belief = 0
+			diana_false_belief = 0
+			allie_false_belief = 0
 			num_speakers = ""
 			coding_experience = ""
 			smartphone_frequency = ""
 			smart_speaker_frequency = ""
 			voice_assistant_frequency = ""
+			current_data = False
 			for row in reader:
 				id = row['child_hashed_id']
+				if row['key'] == "timestamp":
+					study_start = datetime.datetime.strptime('2023-11-22', "%Y-%m-%d")
+					date = datetime.datetime.strptime(row["value"], "%Y-%m-%dT%H:%M:%S.%fZ")
+					if date > study_start: 
+						current_data = True
 				if row['key'] == "videoShown" and row['frame_id'] == "11-alexa-protocol":
 					video = row["value"]
 					matches = re.search("([a-zA-Z]+)-CB([0-9])",video)
@@ -62,35 +65,23 @@ for file in os.listdir(path):
 							test = "wheels"
 						elif value == "right":
 							test = "spider"
-					elif frame_id == "26-null":
+					elif frame_id == "22-null":
 						selectedImage = row['value']
 						if selectedImage == "left":
-							implicit_false_belief = 1
-					elif frame_id == "34-null":
+							attn3 = 1
+					elif frame_id == "25-null":
 						selectedImage = row['value']
 						if selectedImage == "left":
-							diana_false_belief_location = 1
-					elif frame_id == "36-null":
+							rachel_false_belief = 1
+					elif frame_id == "28-null":
 						selectedImage = row['value']
-						if selectedImage == "right":
-							diana_false_belief_control = 1
-					elif frame_id == "38-null":
+						if selectedImage == "left":
+							diana_false_belief = 1
+					elif frame_id == "31-null":
 						selectedImage = row['value']
 						if selectedImage == "middle":
-							allie_false_belief_location = 1
-					elif frame_id == "21-TOM-booklet-2-snack-select":
-						diverse_desires_selected_snack = row['value']
-					elif frame_id == "24-TOM-booklet-3-snack-response":
-						diverse_desires_response_snack = row['value']
-						if diverse_desires_selected_snack != diverse_desires_response_snack:
-							diverse_desires = 1
-					elif frame_id == "29-TOM-booklet-5-backpack-select":
-						diverse_beliefs_selected_location = row['value']
-					elif frame_id == "32-TOM-booklet-6-backpack-response":
-						diverse_beliefs_response_location = row['value']
-						if diverse_beliefs_selected_location != diverse_beliefs_response_location:
-							diverse_beliefs = 1
-				if row['frame_id'] == "46-parent-survey":
+							allie_false_belief = 1
+				if row['frame_id'] == "34-parent-survey":
 					if row['key'] == "formData.num_speakers":
 						num_speakers = row['value']
 					elif row['key'] == "formData.coding_experience":
@@ -104,7 +95,10 @@ for file in os.listdir(path):
 			if test == 0:
 				print("Ignoring data from pilot 1...")
 				continue
-			csv_str = str(id) + "," + condition + "," + counterbalance + "," + str(attn1) + "," + str(attn2) + "," + str(test) + "," + str(diverse_desires) + "," + str(implicit_false_belief) + "," + str(diverse_beliefs) + "," + str(diana_false_belief_location) + "," + str(diana_false_belief_control) + "," + str(allie_false_belief_location) + "," + num_speakers + "," + coding_experience + "," + smartphone_frequency + "," + smart_speaker_frequency + "," + voice_assistant_frequency + "\n"
+			if not current_data:
+				print("Ignoring data from earlier study version")
+				continue
+			csv_str = str(id) + "," + condition + "," + counterbalance + "," + str(attn1) + "," + str(attn2) + "," + str(test) + "," + str(attn3) + "," + str(rachel_false_belief) + "," + str(diana_false_belief) + "," + str(allie_false_belief) + "," + num_speakers + "," + coding_experience + "," + smartphone_frequency + "," + smart_speaker_frequency + "," + voice_assistant_frequency + "\n"
 			data.write(csv_str)
 data.close()
 
